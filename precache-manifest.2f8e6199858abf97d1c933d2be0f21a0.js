ff446ea481d4852a21879b64f03760320faeb153
LesterGallagher <LesterGallagher@gmail.com>
2019-04-28 19:27:13 +0200

    Updates

---
+++ precache-manifest.2f8e6199858abf97d1c933d2be0f21a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85580a21ce7d2a4e7102649150e929ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2355aa1b14573dfc6c29",
+    "url": "./static/css/2.afdf2871.chunk.css"
+  },
+  {
+    "revision": "33701e18d6a4308d63ae",
+    "url": "./static/css/main.656ce512.chunk.css"
+  },
+  {
+    "revision": "2355aa1b14573dfc6c29",
+    "url": "./static/js/2.414c5fe5.chunk.js"
+  },
+  {
+    "revision": "33701e18d6a4308d63ae",
+    "url": "./static/js/main.10181be1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1382c29cdb72f6c99043675d6e13b625",
+    "url": "./static/media/photon-entypo.1382c29c.ttf"
+  },
+  {
+    "revision": "2614e058b2dcb9d6e2e964730d795540",
+    "url": "./static/media/photon-entypo.2614e058.eot"
+  },
+  {
+    "revision": "bf614256dbc49f4bf2cf786706bb0712",
+    "url": "./static/media/photon-entypo.bf614256.woff"
+  }
+]);
